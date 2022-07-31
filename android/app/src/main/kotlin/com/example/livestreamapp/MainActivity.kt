a905cfff9a3eaeb75abc51a04707d21e8e35d697
Asep Oo Kurniawan <kurniawan9411n@gmail.com>
2022-07-31 11:52:00 +0700

    push to git

---
+++ android/app/src/main/kotlin/com/example/livestreamapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.livestreamapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/connect.dart
@@ -0,0 +1,313 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:livekit_client/livekit_client.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+import '../exts.dart';
+import 'room.dart';
+
+class ConnectPage extends StatefulWidget {
+  //
+  const ConnectPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ConnectPageState();
+}
+
+class _ConnectPageState extends State<ConnectPage> {
+  //
+  static const _storeKeyUri = 'uri';
+  static const _storeKeyToken = 'token';
+  static const _storeKeySimulcast = 'simulcast';
+  static const _storeKeyAdaptiveStream = 'adaptive-stream';
+  static const _storeKeyDynacast = 'dynacast';
+  static const _storeKeyFastConnect = 'fast-connect';
+
+  final _uriCtrl = TextEditingController();
+  final _tokenCtrl = TextEditingController();
+  bool _simulcast = true;
+  bool _adaptiveStream = true;
+  bool _dynacast = true;
+  bool _busy = false;
+  bool _fastConnect = false;
+
+  Future<void> _getAppToken() async {
+    final uri = Uri.parse('http://192.168.254.116:5000/join/kurniawan');
+    final response = await http.get(uri);
+    if (response.statusCode != 200) {
+      log('failed to get app token');
+      return;
+    }
+
+    final dynamic data = json.decode(response.body);
+    if (data['ok'] == false) {
+      return;
+    }
+
+    final String token = data['data']['token'].toString();
+    _tokenCtrl.text = token;
+
+    log('New token: $token');
+    await _writePrefs();
+    await _readPrefs();
+
+    return;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _readPrefs();
+  }
+
+  @override
+  void dispose() {
+    _uriCtrl.dispose();
+    _tokenCtrl.dispose();
+    super.dispose();
+  }
+
+  // Read saved URL and Token
+  Future<void> _readPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    _uriCtrl.text = const bool.hasEnvironment('URL')
+        ? const String.fromEnvironment('URL')
+        : prefs.getString(_storeKeyUri) ?? '';
+    _tokenCtrl.text = const bool.hasEnvironment('TOKEN')
+        ? const String.fromEnvironment('TOKEN')
+        : prefs.getString(_storeKeyToken) ?? '';
+    setState(() {
+      _simulcast = prefs.getBool(_storeKeySimulcast) ?? true;
+      _adaptiveStream = prefs.getBool(_storeKeyAdaptiveStream) ?? true;
+      _dynacast = prefs.getBool(_storeKeyDynacast) ?? true;
+      _fastConnect = prefs.getBool(_storeKeyFastConnect) ?? false;
+    });
+  }
+
+  // Save URL and Token
+  Future<void> _writePrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_storeKeyUri, _uriCtrl.text);
+    await prefs.setString(_storeKeyToken, _tokenCtrl.text);
+    await prefs.setBool(_storeKeySimulcast, _simulcast);
+    await prefs.setBool(_storeKeyAdaptiveStream, _adaptiveStream);
+    await prefs.setBool(_storeKeyDynacast, _dynacast);
+    await prefs.setBool(_storeKeyFastConnect, _fastConnect);
+  }
+
+  Future<void> _connect(BuildContext ctx) async {
+    //
+    try {
+      setState(() {
+        _busy = true;
+      });
+
+      // Save URL and Token for convenience
+      await _writePrefs();
+
+      log('Connecting with url: ${_uriCtrl.text}, '
+          'token: ${_tokenCtrl.text}...');
+
+      //create new room
+      final room = Room();
+
+      // Create a Listener before connecting
+      final listener = room.createListener();
+
+      // Try to connect to the room
+      // This will throw an Exception if it fails for any reason.
+      await room.connect(
+        _uriCtrl.text,
+        _tokenCtrl.text,
+        roomOptions: RoomOptions(
+          adaptiveStream: _adaptiveStream,
+          dynacast: _dynacast,
+          defaultVideoPublishOptions: VideoPublishOptions(
+            simulcast: _simulcast,
+          ),
+          defaultScreenShareCaptureOptions: const ScreenShareCaptureOptions(),
+        ),
+      );
+
+      await Navigator.push<void>(
+        ctx,
+        MaterialPageRoute(builder: (_) => RoomPage(room, listener)),
+      );
+    } catch (error, stack) {
+      log('Could not connect $error');
+      log(stack.toString());
+      await ctx.showErrorDialog(error);
+    } finally {
+      setState(() {
+        _busy = false;
+      });
+    }
+  }
+
+  void _setSimulcast(bool? value) async {
+    if (value == null || _simulcast == value) return;
+    setState(() {
+      _simulcast = value;
+    });
+  }
+
+  void _setAdaptiveStream(bool? value) async {
+    if (value == null || _adaptiveStream == value) return;
+    setState(() {
+      _adaptiveStream = value;
+    });
+  }
+
+  void _setDynacast(bool? value) async {
+    if (value == null || _dynacast == value) return;
+    setState(() {
+      _dynacast = value;
+    });
+  }
+
+  void _setFastConnect(bool? value) async {
+    if (value == null || _fastConnect == value) return;
+    setState(() {
+      _fastConnect = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Container(
+          alignment: Alignment.center,
+          child: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 20,
+              ),
+              constraints: const BoxConstraints(maxWidth: 400),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 25),
+                    child: TextField(
+                      controller: _uriCtrl,
+                      decoration: const InputDecoration(hintText: "ws://"),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 25),
+                    child: TextField(
+                      controller: _tokenCtrl,
+                      decoration: const InputDecoration(hintText: "token"),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Simulcast'),
+                        Switch(
+                          value: _simulcast,
+                          onChanged: (value) => _setSimulcast(value),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Adaptive Stream'),
+                        Switch(
+                          value: _adaptiveStream,
+                          onChanged: (value) => _setAdaptiveStream(value),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Fast Connect'),
+                        Switch(
+                          value: _fastConnect,
+                          onChanged: (value) => _setFastConnect(value),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Dynacast'),
+                        Switch(
+                          value: _dynacast,
+                          onChanged: (value) => _setDynacast(value),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 10),
+                    child: ElevatedButton(
+                      onPressed: _busy ? null : () => _getAppToken(),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          if (_busy)
+                            const Padding(
+                              padding: EdgeInsets.only(right: 10),
+                              child: SizedBox(
+                                height: 15,
+                                width: 15,
+                                child: CircularProgressIndicator(
+                                  color: Colors.white,
+                                  strokeWidth: 2,
+                                ),
+                              ),
+                            ),
+                          const Text('GET TOKEN'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: _busy ? null : () => _connect(context),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        if (_busy)
+                          const Padding(
+                            padding: EdgeInsets.only(right: 10),
+                            child: SizedBox(
+                              height: 15,
+                              width: 15,
+                              child: CircularProgressIndicator(
+                                color: Colors.white,
+                                strokeWidth: 2,
+                              ),
+                            ),
+                          ),
+                        const Text('CONNECT'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+}
